ef03e4632c84c2db87a4d0ffc1069ecb3b9ecc12
stass <stass@wix.com>
2015-05-11 12:38:58 +0300

    jdbc - updating imports

--- LearningJDBCModule/src/main/java/BasicJdbcDemo.java
+++ LearningJDBCModule/src/main/java/BasicJdbcDemo.java
@@ -1,11 +1,7 @@
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 /**
  * A sample program that demonstrates how to execute SQL SELECT statement
