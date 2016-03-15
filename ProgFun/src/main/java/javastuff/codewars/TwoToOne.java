5e9692cbc8fad8dc1ff6c9b8ca7c5085e7657f66
stass <stass@wix.com>
2016-03-15 13:03:03 +0200

    TwoToOne

---
+++ ProgFun/src/main/java/javastuff/codewars/TwoToOne.java
@@ -0,0 +1,50 @@
+package javastuff.codewars;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string,
+ * the longest possible, containing distinct letters, - each taken only once - coming from s1 or s2.
+ *
+ * Examples:
+ * a = "xyaabbbccccdefww"
+ * b = "xxxxyyyyabklmopq"
+ * longest(a, b) -> "abcdefklmopqwxy"
+ * a = "abcdefghijklmnopqrstuvwxyz"
+ * longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+ *
+ */
+public class TwoToOne {
+    public static String longest (String s1, String s2) {
+        Set<Character> chars = new TreeSet<>();
+        for(char c1: s1.toCharArray()) {
+            chars.add(c1);
+        }
+
+        for(char c2: s2.toCharArray()) {
+            chars.add(c2);
+        }
+
+        String ans = String.valueOf(chars)
+                .replace(",", "")  //remove the commas
+                .replace("[", "")  //remove the right bracket
+                .replace("]", "")  //remove the left bracket
+                .replace(" ", "")  //remove space
+                .trim();           //remove trailing spaces from partially initialized arrays
+
+        return ans;
+    }
+
+    public static String longest2 (String s1, String s2) {
+        Set<Character> chars = new TreeSet<>();
+        for(char c: (s1 + s2).toCharArray()) {
+            chars.add(c);
+        }
+
+        String ans = String.valueOf(chars)
+                .replaceAll("[\\[\\], ]", "");
+
+        return ans;
+    }
+}
---
+++ ProgFun/src/test/java/codewars/TwoToOneTest.java
@@ -0,0 +1,22 @@
+package codewars;
+
+import static org.junit.Assert.*;
+
+import javastuff.codewars.TwoToOne;
+import org.junit.Test;
+
+public class TwoToOneTest {
+
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests A");
+        assertEquals("aehrsty", TwoToOne.longest("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", TwoToOne.longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", TwoToOne.longest("inmanylanguages", "theresapairoffunctions"));
+
+        System.out.println("longest Fixed Tests B");
+        assertEquals("aehrsty", TwoToOne.longest2("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", TwoToOne.longest2("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", TwoToOne.longest2("inmanylanguages", "theresapairoffunctions"));
+    }
+}
