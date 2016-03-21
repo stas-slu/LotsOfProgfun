ec32b04e3939e0d75c1b5214a1efe61a81361dc5
stass <stass@wix.com>
2016-03-21 09:49:10 +0200

    simple chat demo

---
+++ JavaAndScalaFunctionality/src/main/java/chat/Server.java
@@ -0,0 +1,332 @@
+package chat;
+
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Actually, if you want to run the application in console mode, you only need the first 3 classes.
+ * The two GUI classes can be used as a bonus, it is a very simple GUI. You can run both the Client and the Server
+ * in GUI mode or only one of the two in GUI mode.
+ *
+ * In this application, all the messages sent from the Server to the Client are String objects.
+ * All the messages sent from the Client to the Server (but the first one which is a String) are ChatMessage.
+ * ChatMessage have a type and a String that contains the actual message.
+ *
+ * The Server class.
+ *
+ * You can start the Server by typing
+ * > java Server
+ * at the console prompt. That will execute it in console mode and the server will wait for connection on port 1500.
+ * To use another port pass the port number to use as first parameter to the command
+ * > java Server 1200
+ * will ask the Server to listen on port 1200.
+ * You can use <CTRL>C to stop the server.
+ *
+ * http://www.dreamincode.net/forums/topic/259777-a-simple-chat-program-with-clientserver-gui-optional/
+ */
+/*
+ * The server that can be run both as a console application or a GUI
+ */
+public class Server {
+    // a unique ID for each connection
+    private static int uniqueId;
+    // an ArrayList to keep the list of the Client
+    private ArrayList<ClientThread> clients;
+    // if I am in a GUI
+    private ServerGUI sg;
+    // to display time
+    private SimpleDateFormat sdf;
+    // the port number to listen for connection
+    private int port;
+    // the boolean that will be turned of to stop the server
+    private boolean keepGoing;
+
+    /*
+     *  server constructor that receive the port to listen to for connection as parameter
+     *  in console
+     */
+    public Server(int port) {
+        this(port, null);
+    }
+
+    public Server(int port, ServerGUI sg) {
+        // GUI or not
+        this.sg = sg;
+        // the port
+        this.port = port;
+        // to display hh:mm:ss
+        sdf = new SimpleDateFormat("HH:mm:ss");
+        // ArrayList for the Client list
+        clients = new ArrayList<ClientThread>();
+    }
+
+    public void start() {
+        keepGoing = true;
+		/* create socket server and wait for connection requests */
+        try
+        {
+            // the socket used by the server
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            // infinite loop to wait for connections
+            while(keepGoing)
+            {
+                // format message saying we are waiting
+                display("Server waiting for Clients on port " + port + ".");
+
+                Socket socket = serverSocket.accept();  	// accept connection
+                // if I was asked to stop
+                if(!keepGoing)
+                    break;
+                ClientThread clientThread = new ClientThread(socket);  // make a thread of it
+                clients.add(clientThread);									// save it in the ArrayList
+                clientThread.start();
+            }
+            // I was asked to stop
+            try {
+                serverSocket.close();
+                for(int i = 0; i < clients.size(); ++i) {
+                    ClientThread tc = clients.get(i);
+                    try {
+                        tc.sInput.close();
+                        tc.sOutput.close();
+                        tc.socket.close();
+                    }
+                    catch(IOException ioE) {
+                        // not much I can do
+                    }
+                }
+            }
+            catch(Exception e) {
+                display("Exception closing the server and clients: " + e);
+            }
+        }
+        // something went bad
+        catch (IOException e) {
+            String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
+            display(msg);
+        }
+    }
+    /*
+     * For the GUI to stop the server
+     */
+    protected void stop() {
+        keepGoing = false;
+        // connect to myself as Client to exit statement
+        // Socket socket = serverSocket.accept();
+        try {
+            new Socket("localhost", port);
+        }
+        catch(Exception e) {
+            // nothing I can really do
+        }
+    }
+    /*
+     * Display an event (not a message) to the console or the GUI
+     */
+    private void display(String msg) {
+        String time = sdf.format(new Date()) + " " + msg;
+        if(sg == null)
+            System.out.println(time);
+        else
+            sg.appendEvent(time + "\n");
+    }
+    /*
+     *  to broadcast a message to all Clients
+     */
+    private synchronized void broadcast(String message) {
+        // add HH:mm:ss and \n to the message
+        String time = sdf.format(new Date());
+        String messageLf = time + " " + message + "\n";
+        // display message on console or GUI
+        if(sg == null)
+            System.out.print(messageLf);
+        else
+            sg.appendRoom(messageLf);     // append in the room window
+
+        // we loop in reverse order in case we would have to remove a Client
+        // because it has disconnected
+        for(int i = clients.size(); --i >= 0;) {
+            ClientThread ct = clients.get(i);
+            // try to write to the Client if it fails remove it from the list
+            if(!ct.writeMsg(messageLf)) {
+                clients.remove(i);
+                display("Disconnected Client " + ct.username + " removed from list.");
+            }
+        }
+    }
+
+    // for a client who logoff using the LOGOUT message
+    synchronized void remove(int id) {
+        // scan the array list until we found the Id
+        for(int i = 0; i < clients.size(); ++i) {
+            ClientThread ct = clients.get(i);
+            // found it
+            if(ct.id == id) {
+                clients.remove(i);
+                return;
+            }
+        }
+    }
+
+    /*
+     *  To run as a console application just open a console window and:
+     * > java Server
+     * > java Server portNumber
+     * If the port number is not specified 1500 is used
+     */
+    public static void main(String[] args) {
+        // start server on port 1500 unless a PortNumber is specified
+        int portNumber = 1500;
+        switch(args.length) {
+            case 1:
+                try {
+                    portNumber = Integer.parseInt(args[0]);
+                }
+                catch(Exception e) {
+                    System.out.println("Invalid port number.");
+                    System.out.println("Usage is: > java Server [portNumber]");
+                    return;
+                }
+            case 0:
+                break;
+            default:
+                System.out.println("Usage is: > java Server [portNumber]");
+                return;
+
+        }
+        // create a server object and start it
+        Server server = new Server(portNumber);
+        server.start();
+    }
+
+    /** One instance of this thread will run for each client */
+    class ClientThread extends Thread {
+        // the socket where to listen/talk
+        Socket socket;
+        ObjectInputStream sInput;
+        ObjectOutputStream sOutput;
+        // my unique id (easier for de-connection)
+        int id;
+        // the Username of the Client
+        String username;
+        // the only type of message a will receive
+        ChatMessage chatMessage;
+        // the date I connect
+        String date;
+
+        // Constructore
+        ClientThread(Socket socket) {
+            // a unique id
+            id = ++uniqueId;
+            this.socket = socket;
+			/* Creating both Data Stream */
+            System.out.println("Thread trying to create Object Input/Output Streams");
+            try
+            {
+                // create output first
+                sOutput = new ObjectOutputStream(socket.getOutputStream());
+                sInput  = new ObjectInputStream(socket.getInputStream());
+                // read the username
+                username = (String) sInput.readObject();
+                display(username + " just connected.");
+            }
+            catch (IOException e) {
+                display("Exception creating new Input/output Streams: " + e);
+                return;
+            }
+            // have to catch ClassNotFoundException
+            // but I read a String, I am sure it will work
+            catch (ClassNotFoundException e) {
+            }
+            date = new Date().toString() + "\n";
+        }
+
+        // what will run forever
+        public void run() {
+            // to loop until LOGOUT
+            boolean keepGoing = true;
+            while(keepGoing) {
+                // read a String (which is an object)
+                try {
+                    chatMessage = (ChatMessage) sInput.readObject();
+                }
+                catch (IOException e) {
+                    display(username + " Exception reading Streams: " + e);
+                    break;
+                }
+                catch(ClassNotFoundException e2) {
+                    break;
+                }
+                // the messaage part of the ChatMessage
+                String message = chatMessage.getMessage();
+
+                // Switch on the type of message receive
+                switch(chatMessage.getType()) {
+
+                    case ChatMessage.MESSAGE:
+                        broadcast(username + ": " + message);
+                        break;
+                    case ChatMessage.LOGOUT:
+                        display(username + " disconnected with a LOGOUT message.");
+                        keepGoing = false;
+                        break;
+                    case ChatMessage.WHOISIN:
+                        writeMsg("List of the users connected at " + sdf.format(new Date()) + "\n");
+                        // scan the users connected
+                        for(int i = 0; i < clients.size(); ++i) {
+                            ClientThread ct = clients.get(i);
+                            writeMsg((i+1) + ") " + ct.username + " since " + ct.date);
+                        }
+                        break;
+                }
+            }
+            // remove myself from the arrayList containing the list of the
+            // connected Clients
+            remove(id);
+            close();
+        }
+
+        // try to close everything
+        private void close() {
+            // try to close the connection
+            try {
+                if(sOutput != null) sOutput.close();
+            }
+            catch(Exception e) {}
+            try {
+                if(sInput != null) sInput.close();
+            }
+            catch(Exception e) {};
+            try {
+                if(socket != null) socket.close();
+            }
+            catch (Exception e) {}
+        }
+
+        /*
+         * Write a String to the Client output stream
+         */
+        private boolean writeMsg(String msg) {
+            // if Client is still connected send the message to it
+            if(!socket.isConnected()) {
+                close();
+                return false;
+            }
+            // write the message to the stream
+            try {
+                sOutput.writeObject(msg);
+            }
+            // if an error occurs, do not abort just inform the user
+            catch(IOException e) {
+                display("Error sending message to " + username);
+                display(e.toString());
+            }
+            return true;
+        }
+    }
+}
+
+
